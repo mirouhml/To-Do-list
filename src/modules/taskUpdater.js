671c83fae95649442f95ded418d3f094eafc6fe1
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-22 11:50:57 +0100

    Use object destructring.

--- src/modules/taskUpdater.js
+++ src/modules/taskUpdater.js
@@ -1,7 +1,5 @@
 const updateStatus = (elements, tasksObject, index) => {
-  const checkbox = elements[1];
-  const checkmark = elements[0];
-  const text = elements[2];
+  const [checkmark, checkbox, text] = elements;
   const task = tasksObject.getTasks()[index];
   checkbox.addEventListener('click', () => {
     checkbox.style.display = 'none';
